70955fcc4fea02cf445ebb9e104f743a042c193a
shyshou.maksim <shyshou.maksim@student.ehu.lt>
2025-01-11 22:28:33 +0200

    Final fixes

--- TestProject/NUnitTest.cs
+++ TestProject/NUnitTest.cs
@@ -8,7 +8,9 @@ using AventStack.ExtentReports.Reporter;
 using FluentAssertions;
 using System.Reflection;
 using Serilog;
-using Steps; 
+using Steps;
+using OpenQA.Selenium.DevTools.V129.HeadlessExperimental;
+using System.Linq;
 
 namespace TestProjectNUnit
 {
@@ -19,6 +21,23 @@ namespace TestProjectNUnit
         private ThreadLocal<IWebDriver> driver;
         private ExtentReports extent;
 
+        private static string Screenshot(IWebDriver driver, string testName)
+        {
+            string screenPath = Path.Combine($"Error Screenshot on {DateTime.Now:yyyy.MM.dd_HH-mm-ss}, {testName}.png");
+            ((ITakesScreenshot)driver).GetScreenshot().SaveAsFile(screenPath);
+            return screenPath;
+        }
+
+        private static string Inform()
+        {
+            string result = $"1)OS : {Environment.OSVersion}\n " +
+                $"2)Machine Name : {Environment.MachineName}\n " +
+                $"3)User : {Environment.UserName}\n " +
+                $"4).NET Version : {Environment.Version}\n";
+            return result;
+        }
+
+
         [OneTimeSetUp]
         public void SetupLogging()
         {
@@ -26,15 +45,40 @@ namespace TestProjectNUnit
 
             extent = new ExtentReports();
 
-            if (File.Exists("TestReport.html"))
+            string projectRootPath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\..\..\"));
+
+            string folderName = "LogsAndReports";
+            string outputFolderPath = "";
+            if (File.Exists(Path.Combine(projectRootPath, folderName)))
             {
-                File.Delete("TestReport.html");
+                File.Delete(Path.Combine(projectRootPath, folderName));
+                outputFolderPath = Path.Combine(projectRootPath, folderName);
             }
-            var sparkReporter = new ExtentSparkReporter("TestReport.html");
+            else
+            {
+                outputFolderPath = Path.Combine(projectRootPath, folderName);
+            }
+            
+            Directory.SetCurrentDirectory(outputFolderPath);
+            string currentprocessfolder = Path.Combine(outputFolderPath, $"Test Process on {DateTime.Now:yyyy.MM.dd_HH-mm-ss}");
+            Directory.CreateDirectory(currentprocessfolder);
+            Directory.SetCurrentDirectory(currentprocessfolder);
+
+            string reportfolder = Path.Combine(currentprocessfolder, "Report");
+            Directory.CreateDirectory("Report");
+            Directory.SetCurrentDirectory(reportfolder);
+
+            var sparkReporter = new ExtentSparkReporter(Path.Combine(reportfolder , $"TestReport on {DateTime.Now:yyyy.MM.dd_HH-mm-ss}.html"));
             extent.AttachReporter(sparkReporter);
 
-            Directory.CreateDirectory("NUnitlogs");
-            string logFilePath = "NUnitlogs/tests.log";
+            Directory.SetCurrentDirectory(currentprocessfolder);
+
+
+            string logfolder = Path.Combine(currentprocessfolder, "Log");
+            Directory.CreateDirectory(logfolder);
+            Directory.SetCurrentDirectory(logfolder);
+
+            string logFilePath = $"TestLog on .log";
             if (File.Exists(logFilePath))
             {
                 File.Delete(logFilePath);
@@ -45,6 +89,8 @@ namespace TestProjectNUnit
                 .WriteTo.Console()
                 .WriteTo.File(logFilePath, rollingInterval: RollingInterval.Day)
                 .CreateLogger();
+
+            Directory.SetCurrentDirectory(currentprocessfolder);
         }
 
 
@@ -66,6 +112,8 @@ namespace TestProjectNUnit
 #pragma warning disable IDE0042 // Deconstruct variable declaration
 #pragma warning disable CS8604 // Possible null reference argument.
             ExtentTest passedtest = extent.CreateTest("RunNUnitTest");
+            string info = Inform();
+            passedtest.Info($"Environmental information :{info}");
             var feature = new UserSteps(driver.Value);
             var navigationData = TestDataFactory.NavigationTestData();
             var searchData = TestDataFactory.SearchTestData();
@@ -92,6 +140,8 @@ namespace TestProjectNUnit
         public void RunSkippedTest()
         {
             ExtentTest skippedtest = extent.CreateTest("RunSkippedTest");
+            string info = Inform();
+            skippedtest.Info($"Environmental information :{info}");
             skippedtest.Skip("Test skipped.");
             extent.Flush();
 
@@ -116,15 +166,25 @@ namespace TestProjectNUnit
             var feature = new UserSteps(driver.Value);
 
             ExtentTest failedtest = extent.CreateTest("RunFailTest");
+            string info = Inform();
+            failedtest.Info($"Environmental information :{info}");
             var languageDataFr = TestDataFactory.LanguageTestDataFR();
-            feature.GivenTheUserIsOnTheHomepage();
-            failedtest.Fail("Fail");
-            extent.Flush();
-
-            feature.WhenTheUserSwitchesTheSiteLanguageTo(languageDataFr.Language);
-            feature.ThenTheUserShouldBeRedirectedTo(languageDataFr.ExpectedUrlFragment);
+            try
+            {
+                feature.GivenTheUserIsOnTheHomepage();
+                feature.WhenTheUserSwitchesTheSiteLanguageTo(languageDataFr.Language);
+                feature.ThenTheUserShouldBeRedirectedTo(languageDataFr.ExpectedUrlFragment);
+            }
+            catch (Exception ex)
+            {
+                string screenshotPath = Screenshot(driver.Value, "RunFailTest");
+                failedtest.Fail($"Test fail. Issue message: {ex}")
+                    .AddScreenCaptureFromPath(screenshotPath);
+                throw;
+            }
 
             Log.Information("NUnit Fail test completed.");
+            extent.Flush();
         }
 
         [TearDown]
